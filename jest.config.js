defdb3552c96f8a37ee0c24f655412b93d8681fd
Asel Peiris <asel.p@eyepax.com>
2023-10-03 07:15:40 +0530

    cleaning up

--- jest.config.js
+++ jest.config.js
@@ -6,8 +6,8 @@ module.exports = {
       [
         'jest-qase-reporter',
         {
-          apiToken: 'bfe4276763b5f2d712f30cd68b93348553052c47aa13a34bfd837fbb8e27346b',
-          projectCode: 'DEMO',
+          apiToken: '<api-token>',
+          projectCode: '<project-code>',
           environmentId: 1,
           logging: true,
           runComplete: true,
